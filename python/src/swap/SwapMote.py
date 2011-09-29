8389eac8718df890aadb2c452fb83f142ca369f0
dberenguer@usapiens.com <dberenguer@usapiens.com@d77f469f-0316-7e13-d0a9-da7f422f7f4c>
2011-09-29 15:44:15 +0000

    New folder structure
    git-svn-id: http://panstamp.googlecode.com/svn/trunk@199 d77f469f-0316-7e13-d0a9-da7f422f7f4c

---
+++ python/src/swap/SwapMote.py
@@ -0,0 +1,223 @@
+#########################################################################
+#
+# SwapMote
+#
+# Copyright (c) 2011 Daniel Berenguer <dberenguer@usapiens.com>
+# 
+# This file is part of the panStamp project.
+# 
+# panStamp  is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# panStamp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with panLoader; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 
+# USA
+#
+# Author: Daniel Berenguer
+# Creation date: 20-Aug-2011
+#
+#########################################################################
+__author__="Daniel Berenguer"
+__date__  ="$Aug 20, 2011 10:36:00 AM$"
+#########################################################################
+
+from swap.SwapInfoPacket import SwapInfoPacket
+from swap.SwapCommandPacket import SwapCommandPacket
+from swap.SwapQueryPacket import SwapQueryPacket
+from swap.SwapDefs import SwapRegId, SwapState
+from swap.SwapValue import SwapValue
+from swapexception.SwapException import SwapException
+from xmltools.XmlDevice import XmlDevice
+
+import time
+
+
+class SwapMote(object):
+    """
+    SWAP mote class
+    """
+    def cmdRegister(self, regId, value):
+        """
+        Send command to register and return expected response
+        
+        @param regId: Register ID
+        @param value: New value
+        
+        @return Expected SWAP info packet sent from mote after reception of this command
+        """
+        # Expected response from mote
+        infPacket = SwapInfoPacket(self.address, regId, value)
+        # Command to be sent to the mote
+        cmdPacket = SwapCommandPacket(self.address, regId, value, self.nonce)
+        # Send command
+        cmdPacket.send(self.server.modem)
+        # Return expected response
+        return infPacket;
+
+
+    def qryRegister(self, regId):
+        """
+        Send query to register
+        
+        @param regId: Register ID
+        """
+        # Query packet to be sent
+        qryPacket = SwapQueryPacket(self.address, regId)
+        # Send query
+        qryPacket.send(self.server.modem)
+
+
+    def infRegister(self, regId):
+        """
+        Send SWAP info packet about the current value of the register passed as argument
+        
+        @param regId: Register ID
+        """
+        # Info packet to be sent
+        infPacket = SwapInfoPacket(self.address, regId)
+        # Send SWAP info packet
+        infPacket.send(self.server.modem)
+
+
+    def cmdRegisterWack(self, regId, value):
+        """
+        Send SWAP command to remote register and wait for confirmation
+        
+        @param regId: Register ID
+        @param value: New value
+        
+        @return True if ACK is received from mote. Return False otherwise
+        """
+        return self.server.setMoteRegister(self, regId, value)
+
+
+    def setAddress(self, address):
+        """
+        Set mote address
+        
+        @param address: New mote address
+        
+        @return True if this command is confirmed from the mote. Return False otherwise
+        """
+        val = SwapValue(address, length=1)
+        return self.cmdRegisterWack(SwapRegId.ID_DEVICE_ADDR, val)
+
+
+    def setNetworkId(self, netId):
+        """
+        Set mote's network id. Return true if ACK received from mote
+        
+        @param netId: New Network ID
+        
+        @return True if this command is confirmed from the mote. Return False otherwise
+        """
+        val = SwapValue(netId, length=2)
+        return self.cmdRegisterWack(SwapRegId.ID_NETWORK_ID, val)
+
+
+    def setFreqChannel(self, channel):
+        """
+        Set mote's frequency channel. Return true if ACK received from mote
+        
+        @param channel: New frequency channel
+        
+        @return True if this command is confirmed from the mote. Return False otherwise
+        """
+        val = SwapValue(channel, length=1)
+        return self.cmdRegisterWack(SwapRegId.ID_FREQ_CHANNEL, val)
+
+
+    def setSecurity(self, secu):
+        """
+        Set mote's security option. Return true if ACK received from mote
+        
+        @param secu: Security option
+        
+        @return True if this command is confirmed from the mote. Return False otherwise
+        """
+        val = SwapValue(secu, length=1)
+        return self.cmdRegisterWack(SwapRegId.ID_SECU_OPTION, val)
+
+    
+    def restart(self):
+        """
+        Ask mote to restart
+        
+        @return True if this command is confirmed from the mote. Return False otherwise
+        """
+        val = SwapValue(SwapState.RESTART, length=1)
+        return self.cmdRegisterWack(SwapRegId.ID_SYSTEM_STATE, val)
+
+
+    def leaveSync(self):
+        """
+        Ask mote to leave SYNC mode
+        
+        @return True if this command is confirmed from the mote. Return False otherwise
+        """
+        val = SwapValue(SwapState.STOP, length=1)
+        return self.cmdRegisterWack(SwapRegId.ID_SYSTEM_STATE, val)
+
+    
+    def updateTimeStamp(self):
+        """
+        Update time stamp
+        """
+        self.timeStamp = time.time()
+    
+    
+    def __init__(self, server=None, productCode=None, address=0xFF):
+        """
+        Class constructor
+        
+        @param server: SWAP server object
+        @param productCode: Product Code
+        @param address: Mote address
+        """
+        if server is None:
+            raise SwapException("SwapMote constructor needs a valid SwapServer object")
+        ## Swap server object
+        self.server = server
+        ## Product ID
+        self.productId = 0
+        ## Manufacturer ID
+        self.manufacturerId = 0
+        ## Definition settings
+        self.config = None
+
+        # Get manufacturer and product id from product code
+        if productCode is not None:
+            for i in range(4):
+                self.manufacturerId = self.manufacturerId | (productCode[i] << 8 * (3-i))
+                self.productId = self.productId | (productCode[i + 4] << 8 * (3-i))
+
+        # Definition file
+        ## Definition settings
+        self.definition = XmlDevice(self)
+
+        ## Device address
+        self.address = address
+        ## Current mote's security nonce
+        self.nonce = 0
+        ## State of the mote
+        self.state = SwapState.RUNNING
+        ## List of regular registers provided by this mote
+        self.lstRegRegs = None
+        ## List of config registers provided by this mote
+        self.lstCfgRegs = None
+        if self.definition is not None:
+            # List of regular registers
+            self.lstRegRegs = self.definition.getRegList()
+            # List of config registers
+            self.lstCfgRegs = self.definition.getRegList(config=True)
+        ## Time stamp of the last update received from mote
+        self.timeStamp = None
+
---
+++ python/src/swap/SwapValue.py
@@ -0,0 +1,172 @@
+#########################################################################
+#
+# SwapValue
+#
+# Copyright (c) 2011 Daniel Berenguer <dberenguer@usapiens.com>
+# 
+# This file is part of the panStamp project.
+# 
+# panStamp  is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# panStamp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with panLoader; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 
+# USA
+#
+# Author: Daniel Berenguer
+# Creation date: 20-Aug-2011
+#
+#########################################################################
+__author__="Daniel Berenguer"
+__date__  ="$Aug 20, 2011 10:36:00 AM$"
+#########################################################################
+
+from swapexception.SwapException import SwapException
+
+class SwapValue(object):
+    """
+    Multi-format SWAP value class
+    """
+  
+    def getLength(self):
+        """
+        Get data length
+        
+        @return Length in bytes of the current value
+        """
+        return len(self._data)
+    
+    def toInteger(self):
+        """
+        Convert SWAP value into number
+        
+        @return Current value in integer format
+        """
+        val = 0
+        for i, item in enumerate(self._data):
+            val |= item << (len(self._data)-1-i)*8
+        return val
+
+
+    def clone(self):
+        """
+        Get a copy of the current value
+        
+        @return Copy of the current value
+        """
+        lstData = self._data[:]
+        return SwapValue(lstData)
+
+
+    def toAscii(self):
+        """
+        Convert SWAP value into ASCII string. Use this function for sequences of integer numbers
+        
+        @return Current value in ASCII format
+        """
+        return "".join(str(item) for item in self._data)
+    
+
+    def toAsciiStr(self):
+        """
+        Convert SWAP value into readable ASCII string. Use this function for real ASCII strings
+        
+        @return 
+        """
+        return "".join(chr(item) for item in self._data)
+
+    
+    def toAsciiHex(self):
+        """
+        Convert SWAP value into printable ASCII hex string. Use this function for sequences of
+        integer numbers
+        """
+        out = []
+        for item in self._data:
+            out.append("{0:02X}".format(item))
+        # Return ASCII string
+        return "".join(out)
+
+       
+    def toList(self):
+        """
+        Convert SWAP value into list
+        
+        @return Current value as a list of bytes
+        """
+        return self._data
+
+
+    def isEqual(self, value):
+        """
+        Compare current value with the one passed as argument
+        
+        @param value: Value to be compared agains the current one
+        
+        @return True if the value passed as argument is equal to the current one. Return False
+        otherwise
+        """
+        if self.getLength() == value.getLength():
+            if self._data[:] == value.toList()[:]:
+                return True
+        return False
+
+
+    def __init__(self, value=None, length=0):
+        """
+        Class constructor
+        
+        @param value: Raw value in form of list, boolean, integer, long, string or unicode
+        """
+        ## Raw value in form of list
+        self._data = []
+        isAsciiString = False
+        if value is not None:
+            # In case of list passed in the constructor
+            if type(value) is list:
+                self._data = value
+            elif type(value) is bool:
+                res = int(value)
+            # In case a string is passed in the constructor
+            elif type(value) in [str, unicode]:
+                try:
+                    res = int(value)
+                except ValueError:
+                    # Possible decimal number
+                    dot = value.find(".")
+                    if dot > -1:
+                        try:
+                            integer = int(value[:dot])
+                            numDec = len(value[dot+1:])
+                            decimal = int(value[dot+1:])
+                            res = integer * 10 ** numDec + decimal
+                        except ValueError:
+                            isAsciiString = True
+                    else:
+                        isAsciiString = True
+            else:
+                res = value
+
+            if isAsciiString:
+                # OK, treat value as a pure ASCII string
+                # Leading zeros
+                strLen = len(value)
+                if length > strLen:
+                    for i in range(length - strLen):
+                        self._data.append(0)
+                # Copy string
+                for ch in value:
+                    self._data.append(ord(ch))
+            # In case of integer or long
+            elif length > 0 and length <= 4:
+                for i in range(length):
+                    val = (res >> (8 * (length-1-i))) & 0xFF
+                    self._data.append(val)
