20653997ebe507b7b8363174cdc51715cdd04c0b
dberenguer@usapiens.com <dberenguer@usapiens.com@d77f469f-0316-7e13-d0a9-da7f422f7f4c>
2012-02-21 22:50:36 +0000

    New package structure
    git-svn-id: http://panstamp.googlecode.com/svn/trunk@487 d77f469f-0316-7e13-d0a9-da7f422f7f4c

---
+++ python/pyswap/src/swap/modem/SerialPort.py
@@ -0,0 +1,173 @@
+#########################################################################
+#
+# SerialPort
+#
+# Copyright (c) 2011 Daniel Berenguer <dberenguer@usapiens.com>
+#
+# This file is part of the panStamp project.
+#
+# panStamp  is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# panStamp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with panLoader; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+# USA
+#
+#########################################################################
+__author__="Daniel Berenguer"
+__date__ ="$Aug 21, 2011 17:05:27 AM$"
+#########################################################################
+
+from swap.SwapException import SwapException
+
+import threading
+import serial
+import time, sys
+
+class SerialPort(threading.Thread):
+    """
+    Wrapper class of the pyserial package
+    """
+
+    def run(self):
+        """
+        Run serial port listener on its own thread
+        """
+        self._go_on = True
+        if self._serport is not None:
+            if self._serport.isOpen():
+                # Flush buffers
+                self._serport.flushInput()
+                self._serport.flushOutput()
+                serbuf = []
+                # Listen for incoming serial data
+                while self._go_on:
+                    try:
+                        # Read single byte (non blocking function)
+                        ch = self._serport.read()
+                        if len(ch) > 0:                    
+                            # End of serial packet?
+                            if ch == '\r' or ((ch == '(') and (len(serbuf) > 0)):
+                                strBuf = "".join(serbuf)
+                                serbuf = []
+        
+                                # Enable for debug only
+                                if self._verbose == True:
+                                    print "Rved: " + strBuf
+                                
+                                # Notify reception
+                                if self.serial_received is not None:
+                                    try:
+                                        self.serial_received(strBuf)
+                                    except SwapException as ex:
+                                        ex.display()
+                            elif ch != '\n':
+                                # Append char at the end of the buffer (list)
+                                serbuf.append(ch)
+                    except OSError:
+                        raise SwapException(str(sys.exc_type) + ": " + str(sys.exc_info()))
+           
+                    # Anything to be sent?                   
+                    #self._send_lock.acquire()
+                    if self._strtosend is not None:                  
+                        # Send serial packet
+                        self._serport.write(self._strtosend)                        
+                        # Enable for debug only
+                        if self._verbose == True:
+                            print "Sent: " + self._strtosend
+                        self._strtosend = None
+                    #self._send_lock.release()
+            else:
+                raise SwapException("Unable to read serial port " + self.portname + " since it is not open")
+        else:
+            raise SwapException("Unable to read serial port " + self.portname + " since it is not open")
+
+    
+    def stop(self):
+        """
+        Stop serial port
+        """
+        self._go_on = False
+        if self._serport is not None:
+            if self._serport.isOpen():
+                self._serport.flushInput()
+                self._serport.flushOutput()
+                self._serport.close()
+                
+
+    def send(self, buf):
+        """
+        Send string buffer via serial
+        
+        @param buf: Packet to be transmitted
+        """
+        #self._send_lock.acquire()
+        self._strtosend = buf
+        #self._send_lock.release()
+
+
+    def setRxCallback(self, cb_function):
+        """
+        Set callback reception function. This function is called whenever a new serial packet
+        is received from the gateway
+        
+        @param cb_function: User-defined callback function
+        """
+        self.serial_received = cb_function
+
+
+    def reset(self):
+        """
+        Hardware reset serial modem
+        """
+        # Set DTR line
+        self._serport.setDTR(True)
+        time.sleep(0.1)
+        # Clear DTR line
+        self._serport.setDTR(False)
+
+           
+    def __init__(self, portname="/dev/ttyUSB0", speed=38400, verbose=False):
+        """
+        Class constructor
+        
+        @param portname: Name/path of the serial port
+        @param speed: Serial baudrate in bps
+        @param verbose: Print out SWAP traffic (True or False)
+        """
+        threading.Thread.__init__(self)
+        ## Name(path) of the serial port
+        self.portname = portname
+        ## Speed of the serial port in bps
+        self.portspeed = speed
+        ## Serial port object
+        self._serport = None
+        ## Callback Rx function
+        self.serial_received = None
+        # Strint to send
+        self._strtosend = None
+        self._send_lock = threading.Lock()
+        # Verbose network traffic
+        self._verbose = verbose
+        try:
+            # Open serial port in blocking mode
+            self._serport = serial.Serial(self.portname, self.portspeed, timeout=0)
+            if self._serport is None:
+                raise SwapException("Unable to open serial port" + self.portname)
+            elif not self._serport.isOpen():
+                raise SwapException("Unable to open serial port" + self.portname)
+            # Set to >0 in order to avoid blocking at Tx forever
+            self._serport.writeTimeout = 1
+            # Set DTR line to LOW
+            self._serport.setDTR(False)
+            
+        except serial.SerialException as ex:
+            raise SwapException(str(ex))
---
+++ python/pyswap/src/swap/xmltools/XmlNetwork.py
@@ -0,0 +1,97 @@
+#########################################################################
+#
+# XmlNetwork
+#
+# Copyright (c) 2011 Daniel Berenguer <dberenguer@usapiens.com>
+# 
+# This file is part of the panStamp project.
+# 
+# panStamp  is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# panStamp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with panLoader; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 
+# USA
+#
+#########################################################################
+__author__="Daniel Berenguer"
+__date__ ="$Aug 20, 2011 10:36:00 AM$"
+#########################################################################
+
+import xml.etree.ElementTree as xml
+
+
+class XmlNetwork(object):
+    """
+    Wireless network configuration settings
+    """
+    def read(self):
+        """
+        Read configuration file
+        """
+        # Parse XML file
+        tree = xml.parse(self.file_name)
+        if tree is None:
+            return
+        # Get the root node
+        root = tree.getroot()
+        # Get frequency channel
+        elem = root.find("channel")
+        if elem is not None:
+            self.freq_channel = int(elem.text)
+        # Get Network ID
+        elem = root.find("netid")
+        if elem is not None:
+            self.network_id = int(elem.text, 16)
+        # Get device address
+        elem = root.find("address")
+        if elem is not None:
+            self.devaddress = int(elem.text)
+        # Get device address
+        elem = root.find("security")
+        if elem is not None:
+            self.security = int(elem.text)
+
+
+    def save(self):
+        """
+        Save network settings in file
+        """
+        f = open(self.file_name, 'w')
+        f.write("<?xml version=\"1.0\"?>\n")
+        f.write("<network>\n")
+        f.write("\t<channel>" + str(self.freq_channel) + "</channel>\n")
+        f.write("\t<netid>" + hex(self.network_id) + "</netid>\n")
+        f.write("\t<address>" + str(self.devaddress) + "</address>\n")
+        f.write("\t<security>" + str(self.security) + "</security>\n")
+        f.write("</network>\n")
+        f.close()
+
+    
+    def __init__(self, file_name="network.xml"):
+        """
+        Class constructor
+        
+        @param filename: Path to the network configuration file
+        """
+        ## Name/path of the current configuration file
+        self.file_name = file_name
+        ## Frequency channel
+        self.freq_channel = 0
+        ## Network identifier (synchronization word)
+        self.network_id = 0xB547
+        ## Device address
+        self.devaddress = 1
+        ## Security option
+        self.security = 0
+        # Read XML file
+        self.read()
+  
