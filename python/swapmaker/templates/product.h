0761f54d276e61ee655ae1c8d200e2b00fda1eb5
dberenguer@usapiens.com <dberenguer@usapiens.com@d77f469f-0316-7e13-d0a9-da7f422f7f4c>
2012-05-25 22:44:27 +0000

    First upload
    git-svn-id: http://panstamp.googlecode.com/svn/trunk@575 d77f469f-0316-7e13-d0a9-da7f422f7f4c

---
+++ python/swapmaker/templates/product.h
@@ -0,0 +1,25 @@
+#ifndef _PRODUCT_H
+#define _PRODUCT_H
+
+/**
+ * Hardware version
+ */
+#define HARDWARE_VERSION        0x${HARDWARE_VERSION}
+
+/**
+ * Firmware version
+ */
+#define FIRMWARE_VERSION        0x${FIRMWARE_VERSION}
+
+/**
+ * Manufacturer SWAP ID
+ */
+#define SWAP_MANUFACT_ID        0x${SWAP_MANUFACT_ID}
+
+/**
+ * Product SWAP ID
+ */
+#define SWAP_PRODUCT_ID         0x${SWAP_PRODUCT_ID}
+
+#endif
+
