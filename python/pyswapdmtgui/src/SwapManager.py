f06d59da86ad921576039f65da9aafa15c5a01cc
dberenguer@usapiens.com <dberenguer@usapiens.com@d77f469f-0316-7e13-d0a9-da7f422f7f4c>
2012-09-03 21:06:13 +0000

    Removed some useless comments
    git-svn-id: http://panstamp.googlecode.com/svn/trunk@684 d77f469f-0316-7e13-d0a9-da7f422f7f4c

--- python/pyswapdmtgui/src/SwapManager.py
+++ python/pyswapdmtgui/src/SwapManager.py
@@ -176,9 +176,7 @@ class SwapManager(SwapInterface):
         self.registerValueChanged = None
 
         # wxPython app
-        #self.app = wx.PySimpleApp(0)
         self.app = wx.App(False)
-        #wx.InitAllImageHandlers()
                
         # Start SWAP server
         try:
